538c0d9d11e228bead83efab6cc8b2593ed3162a
Maksim Makushchenko <maksalmak@gmail.com>
2023-11-29 09:49:01 +0300

    Add project files.

---
+++ src/Core/IXlsxSheet.cs
@@ -0,0 +1,27 @@
+﻿using ClosedXML.Excel;
+
+namespace RtuTc.BlazorXlsxExport;
+/// <summary>
+/// Интерфейс листа таблицы, который можно заполнить данными. 
+/// Необходим, так как теоретически страницы могут содержать разеородные данные и лист не должен быть generic
+/// Интерфейс объявлен как internal для сокрытия используемой библиотеки для работы с excel
+/// </summary>
+internal interface IXlsxSheet
+{
+    Task AddSheetToWorkbook(XLWorkbook xLWorkbook);
+}
+
+internal class IXlsxWrapper : IXlsxSheet
+{
+    private readonly Func<XLWorkbook, Task> _action;
+
+    public IXlsxWrapper(Func<XLWorkbook, Task> action)
+    {
+        _action = action;
+    }
+
+    public async Task AddSheetToWorkbook(XLWorkbook xLWorkbook)
+    {
+        await _action(xLWorkbook);
+    }
+}
---
+++ src/Core/Options/XlsxSheetOption.cs
@@ -0,0 +1,28 @@
+﻿using ClosedXML.Excel;
+
+using Microsoft.AspNetCore.Components;
+
+namespace RtuTc.BlazorXlsxExport.Options;
+[CascadingTypeParameter(nameof(TItem))]
+public abstract class XlsxSheetOption<TItem> : ComponentBase, IDisposable
+{
+    [CascadingParameter]
+    public XlsxSheet<TItem>? Sheet { get; set; }
+
+    internal abstract void ApplyOption(IXLWorksheet worksheet);
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        if (Sheet is null)
+        {
+            throw new InvalidOperationException($"{nameof(XlsxSheetOption<TItem>)} can be used only in child content of {nameof(XlsxSheet<TItem>)}");
+        }
+        Sheet.AddOption(this);
+    }
+
+    public void Dispose()
+    {
+        Sheet?.RemoveOption(this);
+    }
+}
